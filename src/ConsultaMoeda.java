da4ada4cdaa35d6cfa6dfaf068d7981e4becbbb5
Leilanny Rodrigues <leilannyrodrigues@gmail.com>
2025-12-29 09:17:37 -0300

    Entrega-do-Challenge

---
+++ src/ConsultaMoeda.java
@@ -0,0 +1,24 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaMoeda {
+    public String buscaMoeda(String moedaBase) {
+
+        String chave = "7b1dfb6c770a5a65a55bfe31";
+        URI endereco = URI.create("https://v6.exchangerate-api.com/v6/" + chave + "/latest/" + moedaBase);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(endereco).build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar a API.");
+        }
+    }
+}
+
+
